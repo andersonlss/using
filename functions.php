a70507841e12e919cdf28ed232576445b9ac74f3
Anderson Luis dos Santos Selenguini <anderson.selenguini@gmail.com>
2015-11-09 00:43:08 -0200

    update title

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title> Funcs </title>
+        <title> Funcionalidades </title>
     </head>
     <body>
         <?php
