85bc0098cb5a1067addfbb60a9535ad053db5f77
Anderson Luis dos Santos Selenguini <anderson.selenguini@gmail.com>
2015-11-09 00:02:49 -0200

    1º Commit - Arquivos Base

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Ola Mundo </title>
+    </head>
+    <body>
+        <?php
+            $x=2;
+            while ($x<10) {
+                $x+=2;
+            }
+            echo $x;
+            echo "<br>Eai";
+        ?>
+    </body>
+</html>
